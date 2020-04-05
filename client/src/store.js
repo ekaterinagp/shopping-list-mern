25607c6d10144f29f5957b6ac25432116cb7d629
ekaterinagp <katya.koriakina@gmail.com>
2020-04-05 09:18:18 +0200

    start redux

---
+++ client/src/store.js
@@ -0,0 +1,31 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const initialState = {};
+
+const middleware = [thunk];
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware)
+//     //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
+const devtools =
+  process.env.NODE_ENV === "test"
+    ? (x) => x /* eslint-disable no-underscore-dangle */
+    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__();
+/* eslint-enable no-underscore-dangle */
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(thunk), devtools)
+);
+
+export default store;
