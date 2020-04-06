53bacb4f86d893cb62d25af4409a77ab3be099be
ekaterinagp <katya.koriakina@gmail.com>
2020-04-06 16:26:35 +0200

    fix store redux

--- client/src/store.js
+++ client/src/store.js
@@ -4,28 +4,28 @@ import rootReducer from "./reducers";
 
 const initialState = {};
 
-// const middleware = [thunk];
-
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(...middleware)
-//     //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// );
-
-const devtools =
-  process.env.NODE_ENV === "test"
-    ? (x) => x /* eslint-disable no-underscore-dangle */
-    : window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__();
-/* eslint-enable no-underscore-dangle */
+const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(thunk), devtools)
+  compose(
+    applyMiddleware(...middleware)
+    //  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
+
+// const devtools =
+//   process.env.NODE_ENV === "test"
+//     ? (x) => x /* eslint-disable no-underscore-dangle */
+//     : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//       window.__REDUX_DEVTOOLS_EXTENSION__();
+// /* eslint-enable no-underscore-dangle */
+
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(applyMiddleware(thunk), devtools)
+// );
 
 export default store;
