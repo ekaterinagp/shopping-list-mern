ad8c4bc7594c0ffea8c758a2f2326bfa9c14505e
ekaterinagp <katya.koriakina@gmail.com>
2020-04-08 11:43:22 +0200

    add tokens for users and items jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const config = require("config");
 const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
 
 //Item Model
 const User = require("../../models/User");
@@ -42,6 +43,13 @@ router.post("/", (req, res) => {
       );
     });
   });
+});
+
+//@route GET api/auth/user - get user data, private route
+router.get("/user", auth, (req, res) => {
+  User.findById(req.user.id)
+    .select("-password")
+    .then((user) => res.json(user));
 });
 
 module.exports = router;
